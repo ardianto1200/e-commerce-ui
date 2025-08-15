600e6fc6a91f589646e9e532921ddc4cd35b8689
ardianto1200 <ardianto1200@gmail.com>
2025-08-16 01:00:44 +0700

    update sampai product list

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { ProductType } from "@/types";
+
+const ProductCard = ({ product }: { product: ProductType }) => {
+  return (
+    <div className="w-full">Product Card</div>
+  );
+};
+
+export default ProductCard;
